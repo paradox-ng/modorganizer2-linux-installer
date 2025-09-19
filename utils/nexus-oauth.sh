6158b771cb251a9ab7b17bd5126f54f5ebb9495d
Furglitch <samsonger@proton.me>
2025-09-18 23:21:07 -0500

    Adjust code challenge creation, add URL generation

--- utils/nexus-oauth.sh
+++ utils/nexus-oauth.sh
@@ -3,23 +3,25 @@
 function code_challenge() { # https://modding.wiki/en/api/oauth2-guide#create-the-code-challenge-public-apps
 	verifier="$(openssl rand -hex 43)"
 
-	base64url() {
-		echo -n "$1" | openssl base64 -e | tr '+/' '-_' | tr -d '=' | tr -d '\n'
-	}
-
-	challenge="$(echo -n "$verifier" | openssl dgst -sha256 -binary | base64url)"
+	challenge="$(printf '%s' "$verifier" | openssl dgst -sha256 -binary | openssl base64 -A | tr '+/' '-_' | tr -d '=')"
 
 	echo "$challenge"
 }
 
 function auth_url() { # https://modding.wiki/en/api/oauth2-guide#generate-an-authorize-url
 
-	clientID='PLACEHOLDER'
+	client_id='PLACEHOLDER'
 	response_type='code'
 	scope=''
 	redirect_uri='PLACEHOLDER'
 	state=$(uuidgen)
 	method='S256'
 	challenge=$(code_challenge)
+
+	url="https://users.nexusmods.com/oauth/authorize?client_id=${client_id}&response_type=${response_type}&scope=${scope}&redirect_uri=${redirect_uri}&state=${state}&code_challenge_method=${method}&code_challenge=${challenge}"
+
+	echo "$url"
 	
 }
+
+echo $(auth_url)
