9a79849c0ab03112cfe9ca36bdf729e7165f2b9c
Blake F Lowe <65426013+daemon-mouse@users.noreply.github.com>
2025-10-04 01:20:14 -0400

    fallout4: add GOG product id

--- gamesinfo/fallout4.sh
+++ gamesinfo/fallout4.sh
@@ -1,7 +1,7 @@
 game_steam_subdirectory="Fallout 4"
 game_nexusid="fallout4"
 game_appid=377160
-game_gog_productid=""
+game_gog_productid=1998527297
 game_epic_productid=""
 game_executable="Fallout4Launcher.exe"
 game_protontricks=("xaudio2_7=native" "grabfullscreen=y")
