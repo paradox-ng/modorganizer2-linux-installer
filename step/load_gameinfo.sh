139f50e392a512ebb6892d400a4911729ea850cb
Blake F Lowe <65426013+daemon-mouse@users.noreply.github.com>
2025-10-04 20:59:31 -0400

    heroic: load game info for GOG and Epic games
    This introduces the new helper script find-heroic-game-installation.sh
    to find games installed by Heroic. It does a bit more work than its
    sister script for Steam, find_library_for_file.sh; namely, it looks up
    the Wine version and prefix folder set for the game. We need these
    because we can't use protontricks later.
    
    In load_gameinfo.sh, we now search in both Steam and Heroic locations to
    find the selected game. The current logic is short-circuiting, so if a
    Steam game is found, Heroic will not be checked. We could consider
    checking both and allowing the user to choose which version they want;
    however, I think the likelihood of someone installing the same game in
    two different launchers is low enough to not justify the extra code.
    
    This commit downgrades an absent appid from an error to a warning. This
    allows us to incrementally add support for GOG/Epic games before adding
    Steam support.
    
    We set a new variable, game_launcher, which has the value of "steam" or
    "heroic", allowing other scripts to implement different logic for each
    launcher.

--- step/load_gameinfo.sh
+++ step/load_gameinfo.sh
@@ -15,38 +15,56 @@ fi
 
 source "$load_gameinfo"
 
-if [ -z "$game_appid" ]; then
-	log_error "empty game_appid"
-	exit 1
-elif [ -z "$game_steam_subdirectory" ]; then
-	log_error "empty steam_subdirectory"
+find_steam_game() {
+	if [ -z "$game_appid" ]; then
+		log_warn "empty game_appid"
+		return 1
+	elif [ -z "$game_steam_subdirectory" ]; then
+		log_warn "empty steam_subdirectory"
+		return 1
+	fi
+
+	steam_library=$(
+		export game_appid game_steam_subdirectory game_executable dialog
+		"$utils/find-library-for-file.sh"
+	) ||
+		case "$?" in
+		1)
+			log_info "could not find any Steam library containing a game with appid '$game_appid'. If this game is installed with Steam and you know exactly where the library is, you can specify it using the environment variable STEAM_LIBRARY"
+			return 1
+			;;
+		2)
+			# Fallout 3 needs to be downgraded, no additional dialogs to show
+			exit 1
+			;;
+		esac
+	game_launcher=steam
+	game_installation="$steam_library/steamapps/common/$game_steam_subdirectory"
+	return 0
+}
+
+find_heroic_game() {
+	if ! heroic_vars=$(
+		export game_gog_productid game_epic_productid
+		"$utils/find-heroic-game-installation.sh"
+	); then
+		return 1
+	fi
+	game_launcher=heroic
+	eval "$heroic_vars"
+}
+
+if ! find_steam_game && ! find_heroic_game; then
+	"$dialog" errorbox \
+		"Could not find '$selected_game' in any of your Steam or Heroic folders.\nMake sure the game is installed and that you've run it at least once."
 	exit 1
 fi
-
-steam_library=$(
-	export game_appid game_steam_subdirectory game_executable dialog
-	"$utils/find-library-for-file.sh"
-) ||
-	case "$?" in
-	1)
-		log_error "could not find any Steam library containing a game with appid '$game_appid'. If you know exactly where the library is, you can specify it using the environment variable STEAM_LIBRARY"
-		"$dialog" errorbox \
-			"Could not find '$game_steam_subdirectory' in any of your Steam libraries\nMake sure the game is installed and that you've run it at least once"
-		exit 1
-		;;
-	2)
-		# Fallout 3 needs to be downgraded, no additional dialogs to show
-		exit 1
-		;;
-	esac
 
 if [ "$game_scriptextender_url" != "" ]; then
 	hasScriptExtender=true
 else
 	hasScriptExtender=false
 fi
-
-game_installation="$steam_library/steamapps/common/$game_steam_subdirectory"
 
 # defer loading these variables to step/clean_game_prefix.sh
 game_prefix=''
---
+++ utils/find-heroic-game-installation.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+function log_info() {
+	echo "INFO:" "$@" >&2
+}
+
+function log_warn() {
+	echo "WARN:" "$@" >&2
+}
+
+function log_error() {
+	echo "ERROR:" "$@" >&2
+}
+
+get_wine_variables() {
+	local productid="$1"
+	local game_config_json="$heroic_config_directory/GamesConfig/$productid.json"
+	if [ ! -f "$game_config_json" ]; then
+		return 1
+	fi
+
+	if ! heroic_game_wine="$(
+		jq --exit-status --raw-output \
+			".\"$productid\".wineVersion.bin" "$game_config_json"
+	)"; then
+		log_error "wine version is unset for this game"
+		return 1
+	fi
+	log_info "found Heroic WINE at '$heroic_game_wine'"
+
+	if ! heroic_game_wineprefix="$(
+		jq --exit-status --raw-output \
+			".\"$productid\".winePrefix" "$game_config_json"
+	)"; then
+		log_error "wine prefix is unset for this game"
+		return 1
+	fi
+	log_info "found Heroic WINEPREFIX at '$heroic_game_wineprefix'"
+}
+
+find_gog_game() {
+	local gog_installed_json="$heroic_config_directory/gog_store/installed.json"
+	if [ -z "$game_gog_productid" ]; then
+		log_warn "empty game_gog_productid"
+		return 1
+	fi
+
+	if [ ! -f "$gog_installed_json" ] || ! game_installation="$(
+		jq --exit-status --raw-output \
+			".installed[] | select(.appName == \"$game_gog_productid\") | .install_path" \
+			"$gog_installed_json"
+	)"; then
+		log_info "Heroic GOG game not installed"
+		return 1
+	fi
+	log_info "found Heroic GOG installation at '$game_installation'"
+
+	get_wine_variables "$game_gog_productid"
+}
+
+find_epic_game() {
+	local epic_installed_json="$heroic_config_directory/legendaryConfig/legendary/installed.json"
+	if [ -z "$game_epic_productid" ]; then
+		log_warn "empty game_epic_productid"
+		return 1
+	fi
+
+	if [ ! -f "$epic_installed_json" ] || ! game_installation="$(
+		jq --exit-status --raw-output \
+			".\"$game_epic_productid\".install_path" \
+			"$epic_installed_json"
+	)"; then
+		log_info "Heroic Epic game not installed"
+		return 1
+	fi
+	log_info "found Heroic Epic installation at '$game_installation'"
+
+	get_wine_variables "$game_epic_productid"
+}
+
+heroic_install_candidates=(
+	"$HOME/.config/heroic"
+	"$HOME/.var/app/com.heroicgameslauncher.hgl/config/heroic"
+)
+
+for heroic_config_directory in "${heroic_install_candidates[@]}"; do
+	if [ -d "$heroic_config_directory" ]; then
+		log_info "found Heroic in '$heroic_config_directory'"
+
+		if find_gog_game || find_epic_game; then
+			echo "${game_installation@A}"
+			echo "${heroic_game_wine@A}"
+			echo "${heroic_game_wineprefix@A}"
+			exit 0
+		fi
+	fi
+done
+exit 1
