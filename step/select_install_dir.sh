43ce527faef3d2437332448fe0eb2a1f8acf8390
Blake F Lowe <65426013+daemon-mouse@users.noreply.github.com>
2025-10-05 03:05:23 -0400

    select_install_dir.sh: use ~/Games/Heroic as default path with heroic
    This is for a nuanced reason: the flatpak version of Heroic can't run
    MO2 when it is installed to ~/Games/mod-organizer-2-*. See the added
    comment for more info.

--- step/select_install_dir.sh
+++ step/select_install_dir.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-picker_text=$( \
-cat << EOF
+picker_text=$(
+	cat <<EOF
 Select where you would like to install Mod Organizer 2
 EOF
 )
 
-screen_text=$( \
-cat << EOF
+screen_text=$(
+	cat <<EOF
 The selected directory is outside of your /home directory.
 
 This should not cause issues, as long as you make sure Steam has permission to access this folder by setting the STEAM_COMPAT_MOUNTS argument in the game's properties.
@@ -15,11 +15,24 @@ More information can be found in the Post-Installation instruction on GitHub.
 EOF
 )
 
-directory=$( \
+directory=$(
+	case "$game_launcher" in
+	steam)
+		default_directory="$HOME/Games/mod-organizer-2-${game_nexusid}"
+		;;
+	heroic)
+		# In the Heroic official flatpak, com.heroicgameslauncher.hgl,
+		# the launcher is not permitted to read outside the
+		# ~/Games/Heroic folder. So, if we don't install here, we will
+		# cause the steam-redirector to fail after installation.
+		default_directory="$HOME/Games/Heroic/mod-organizer-2-${game_nexusid}"
+		;;
+
+	esac
 	"$dialog" \
 		directorypicker \
 		"$picker_text" \
-		"$HOME/Games/mod-organizer-2-${game_nexusid}" \
+		"$default_directory"
 )
 
 if [ -z "$directory" ]; then
@@ -28,12 +41,11 @@ if [ -z "$directory" ]; then
 fi
 
 if [[ "$directory" != $HOME/* ]]; then
-	button=$( \
+	button=$(
 		"$dialog" \
-		infobox \
-		"$screen_text"
+			infobox \
+			"$screen_text"
 	)
 fi
 
 echo "$directory"
-
