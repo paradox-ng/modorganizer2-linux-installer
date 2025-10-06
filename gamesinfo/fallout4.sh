6d5b3a5cf72f707c3d883c2062f9bf5abdf32643
Blake F Lowe <65426013+daemon-mouse@users.noreply.github.com>
2025-10-06 18:54:19 -0400

    Fallout 4: add F4SE url for GOG release

--- gamesinfo/fallout4.sh
+++ gamesinfo/fallout4.sh
@@ -5,5 +5,13 @@ game_gog_productid=1998527297
 game_epic_productid=""
 game_executable="Fallout4Launcher.exe"
 game_protontricks=("xaudio2_7=native" "grabfullscreen=y")
-declare -A game_scriptextender_urls=()
-game_scriptextender_files=""
+declare -A game_scriptextender_urls=(
+	[gog]="https://f4se.silverlock.org/beta/f4se_0_06_23.7z" # Fallout 4 runtime 1.10.163
+)
+game_scriptextender_files=(
+	"f4se_0_06_23/Data"
+	"f4se_0_06_23/CustomControlMap.txt"
+	"f4se_0_06_23/f4se_1_10_163.dll"
+	"f4se_0_06_23/f4se_loader.exe"
+	"f4se_0_06_23/f4se_steam_loader.dll"
+)
