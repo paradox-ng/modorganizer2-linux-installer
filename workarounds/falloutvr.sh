1a82bdde1e05b0eab0bdeecf46acc537f8c9110e
furglitch <samsonger@proton.me>
2025-11-18 20:38:09 -0600

    Fix incorrect ini filename
    Resolves #867

--- workarounds/falloutvr.sh
+++ workarounds/falloutvr.sh
@@ -4,7 +4,7 @@ f4se_config_dir="$game_installation/Data/F4SE"
 
 mkdir -p "$f4se_config_dir"
 
-cat << EOT >> "$f4se_config_dir/F4SE.ini"
+cat << EOT >> "$f4se_config_dir/F4SEVR.ini"
 [Loader]
 RuntimeName=_Fallout4VR.exe
 EOT
